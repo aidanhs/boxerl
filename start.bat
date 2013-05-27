2bcff0477fce4e49027cb11754109e002f26bb6f
Aidan Hobson Sayers <aidanhs@cantab.net>
2013-05-27 20:31:26 +0100

    Correct path for erl.exe

--- start.bat
+++ start.bat
@@ -1,2 +1,1 @@
-.\erl5.10.1\erts-5.10.1\bin\erl.exe -pa ebin deps\mimetypes\ebin deps\cowboy\ebin deps\ranch\ebin -s websocket
-
+.\erl\erts-5.10.1\bin\erl.exe -pa ebin deps\mimetypes\ebin deps\cowboy\ebin deps\ranch\ebin -s websocket
