d24e5825d9e028c2fb118d3e3e55795187f67d83
Aidan Hobson Sayers <aidanhs@cantab.net>
2013-06-01 16:04:55 +0100

    Add lock_after function

--- src/ws_handler.erl
+++ src/ws_handler.erl
@@ -19,6 +19,20 @@ websocket_handle({text, Cmd}, Req, State) ->
 		<<"lock">> ->
 			lock_computer(),
 			{reply, {text, <<"Computer locked">>}, Req, State};
+		<<"lock_after ", Time/binary>> ->
+			case parse_num(Time) of
+				error ->
+					{reply, {text, <<Time/binary, " not a time">>}, Req, State};
+				Seconds ->
+					case whereis(lock_timer) of
+						undefined ->
+							Pid = spawn(fun() -> lock_after(Seconds) end),
+							register(lock_timer, Pid);
+						_Pid ->
+							lock_timer ! Seconds
+					end,
+					{reply, {text, <<"Locking in ", Time/binary, " secs">>}, Req, State}
+			end;
 		Other ->
 			{reply, {text, <<"Unknown command ", Other/binary>>}, Req, State}
 	end;
@@ -33,5 +47,31 @@ websocket_terminate(_Reason, _Req, _State) ->
 
 %% UTILITY FUNCTIONS DOWN HERE %%
 
+parse_num(<<"inf">>) -> inf;
+parse_num(Bin) ->
+	Str = binary_to_list(Bin),
+	case string:to_integer(Str) of
+		{N,[]} ->
+			N;
+		_ ->
+			error
+	end.
+
 lock_computer() ->
 	os:cmd("rundll32.exe user32.dll, LockWorkStation").
+
+lock_after(Seconds) ->
+	receive
+		NewSeconds ->
+			lock_after(NewSeconds)
+	after
+		5000 ->
+			case Seconds of
+				inf ->
+					ok;
+				N when N > 0 ->
+					lock_after(N - 5);
+				N when N =< 0 ->
+					lock_computer()
+			end
+	end.
